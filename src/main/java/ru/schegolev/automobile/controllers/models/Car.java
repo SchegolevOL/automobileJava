df76a8ca74a9b6de93c53846bf3e073da9d4e840
Oleg Schegol <olschegolev@yahoo.com>
2023-12-02 12:43:47 +0500

    add db

---
+++ src/main/java/ru/schegolev/automobile/controllers/models/Car.java
@@ -0,0 +1,32 @@
+package ru.schegolev.automobile.controllers.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Cars")
+public class Car {
+    public enum Type{
+        Sedan, Hatchback, StationWagon
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String manufacturer;
+    private  String model;
+    private double engineCapacity;
+    private int age;
+    private  String color;
+    @Enumerated(value = EnumType.STRING)
+    private Type type;
+
+}
